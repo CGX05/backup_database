e959df8c8132c29017d14df88f3cde675f8cf80d
CGX <2717630445@qq.com>
2025-09-17 14:43:39 +0800

    修改.github/workflows/deploy.yml

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         app="main:app",
-        host='127.0.0.1',
-        port=8080,
+        host='0.0.0.0',
+        port=8000,
         reload=True
     )
