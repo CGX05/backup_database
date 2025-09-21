10e0debbdfd14b22f2253ac13eb3c6076e05faab
CGX <2717630445@qq.com>
2025-09-22 00:35:04 +0800

    添加json文件，添加接口检查的逻辑

--- main.py
+++ main.py
@@ -56,11 +56,11 @@ async def get_health_status():
     return all_check
 
 
-if __name__ == "__main__":
-
-    uvicorn.run(
-        app="main:app",
-        host='127.0.0.1',
-        port=8000,
-        reload=True
-    )
+# if __name__ == "__main__":
+#
+#     uvicorn.run(
+#         app="main:app",
+#         host='127.0.0.1',
+#         port=8000,
+#         reload=True
+#     )
